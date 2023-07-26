dc43b5ec54315859b4e056e0e7fd998842e6ef8f
Minh Kỳ <minhky.book@gmail.com>
2023-07-26 11:00:19 +0700

    new committ

---
+++ index.js
@@ -0,0 +1,17 @@
+// Imports the express module
+const express = require('express');
+
+// Create a new app instance
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send({hi: 'there'});
+});
+
+// Dynamic port
+const PORT = process.env.PORT || 5000;
+
+// 
+app.listen(PORT, ()=>{
+    console.log('Server running on port ' + PORT);
+});
