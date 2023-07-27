ad02968eaa2a5317402c49179e9451d0add61a0a
Minh Kỳ <minhky.book@gmail.com>
2023-07-27 12:19:57 +0700

    New structure for system

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 // Imports modules
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("./services/passport");
+const keys = require("./config/key");
+
+// Connect to MongoDB
+mongoose.connect(keys.mongoURI);
 
 // Create a new app instance
 const app = express();
 
-// Use passport and google strategy
-passport.use(new GoogleStrategy());
+require("./routes/authRoute")(app);
+
+
+
 
 // Dynamic port
 const PORT = process.env.PORT || 5000;
 
-// 
-app.listen(PORT, ()=>{
-    console.log('Server running on port ' + PORT);
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
