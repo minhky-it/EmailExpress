3dbf0491876bc28e58f3be35b08517f1d793b1b8
Minh Kỳ <minhky.book@gmail.com>
2023-09-06 10:34:58 +0700

    Update key.js

--- config/key.js
+++ config/key.js
@@ -2,7 +2,8 @@ if(process.env.NODE_ENV === 'production'){
     // We're in production - return the prod set of keys
     module.exports = require('./prod');
 
-}else{
-    // We're in development - return the dev keys
-    module.exports = require('./dev');
 }
+// else{
+//     // We're in development - return the dev keys
+//     module.exports = require('./dev');
+// }
